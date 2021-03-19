391647fa14c14aae08810381cc53b9712b0915d5
soapjax <80938889+soapjax@users.noreply.github.com>
2021-03-19 12:29:51 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; Voltaic Mart</Col>
         </Row>
       </Container>
     </footer>
